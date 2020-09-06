85f871e422f3b472f3f7ec3ab2e81593055d01b0
Evan Kohout <ebox86@gmail.com>
2020-09-06 11:35:50 -0700

    fixed margin and padding on homepage for mobile

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,27 +6,26 @@ export default () => {
   return (
     <Layout>
       <main className="max-w-4xl flex-grow mx-auto flex flex-col justify-around">
-        <div className="sm:flex sm:flex-row-reverse sm:items-center">
+        <div className="sm:flex sm:flex-row sm:items-center">
+          <img
+              className="w-full max-w-lg mt-16 mx-auto sm:w-1/2"
+              src={outcast_trading_logo}
+              alt="Outcast trading"
+            />
           <div className="sm:px-2">
-            <h1 className="px-4 pt-5 text-2xl text-left text-red-700 font-bold sm:text-3xl">
-              Outcast Trading Group
+            <h1 className="px-4 pt-5 text-2xl text-left text-red-700 font-bold sm:text-4xl">
+              Outcast Trading
             </h1>
             <p className="px-4 mt-8 mb-8 text-lg text-gray-700 sm:mt-8">
               A trading group with attitude.
             </p>
             <a
             href="https://discord.gg/cpQnZk"
-            className="px-12 py-4  bg-red-700 text-white text-xs rounded shadow hover:bg-red-400"
-          >
+            className="mx-4 px-12 py-4  bg-red-700 text-white text-xs rounded shadow hover:bg-red-400"
+            >
             Join on Discord
-          </a>
+            </a>
           </div>
-          <img
-            className="w-full max-w-lg mt-16 mx-auto sm:w-1/2"
-            src={outcast_trading_logo}
-            alt="Outcast trading"
-          />
-
         </div>
       </main>
     </Layout>
