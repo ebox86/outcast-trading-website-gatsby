a94cc84119d7d00ea1eca0d164bb1902a0e0990e
Evan Kohout <ebox86@gmail.com>
2020-09-19 16:30:44 -0700

    added .env example file, reverted login changes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,6 +1,18 @@
 import "./src/css/tailwind.css"
-import { Auth } from "aws-amplify"
+import Amplify, { Auth } from "aws-amplify"
+import awsConfig from "./src/aws-exports"
 import { setUser } from "./src/utility/auth"
+
+const oauth = {
+    domain: process.env.COGNITO_AUTH_DOMAIN,
+    scope: ['openid', 'profile'],
+    redirectSignIn: process.env.COGNITO_REDIRECT_SIGNIN,
+    redirectSignOut: process.env.COGNITO_REDIRECT_SIGNOUT,
+    responseType: 'token'
+  };
+
+Amplify.configure(awsConfig);
+Auth.configure({ oauth });
 
 export const onRouteUpdate = (state, page, pages) => {
   Auth.currentAuthenticatedUser()
